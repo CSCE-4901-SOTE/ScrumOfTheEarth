8a518684078d25ed59e34fa40df9a0e426bea4a7
cmccoy0603 <cvmccoy123@gmail.com>
2025-10-05 22:26:02 -0500

    Change structure and add springboot backend

---
+++ backend/src/test/java/com/sote/FarmRa/FarmRaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sote.FarmRa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmRaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
