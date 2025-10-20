57781a1bef8c6b8c6d72ed446d10a1a184eebf4e
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2025-10-20 17:24:22 -0500

    Add routes for signup and login pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signup-page', component: SignupPageComponent },
+  { path: 'login-page', component: LoginPageComponent },
+  { path: '', redirectTo: '/signup-page', pathMatch: 'full' }
+];
