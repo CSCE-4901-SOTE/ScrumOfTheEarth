8157c54229d94e24ce171d26883fae3f34d81a0b
blakezadeh <blakezadeh@gmail.com>
2025-10-21 13:35:00 -0500

    add topbar for dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { SignupPageComponent } from './signup-page/signup-page.component';
+//import { SignupPageComponent } from './signup-page/signup-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: 'signup-page', component: SignupPageComponent },
+//  { path: 'signup-page', component: SignupPageComponent },
   { path: 'login-page', component: LoginPageComponent },
+  { path: 'dashboard', component: DashboardComponent},
   { path: '', redirectTo: '/signup-page', pathMatch: 'full' }
 ];
