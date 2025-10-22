8f4beeed92f15a0bc11be43fc75824cb89f34233
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2025-10-22 17:54:55 -0500

    Change default redirect to 'signup-page'
    Updated default redirect path to 'signup-page'.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,5 +5,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 export const routes: Routes = [
   { path: 'signup-page', component: SignupPageComponent },
   { path: 'login-page', component: LoginPageComponent },
-  { path: '', redirectTo: '/signup-page', pathMatch: 'full' }
+  { path: '', redirectTo: 'signup-page', pathMatch: 'full' } // default opens login
 ];
+
+
