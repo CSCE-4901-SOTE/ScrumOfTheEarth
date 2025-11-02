f32a40e8a02bf9bca7509db9fb81edb579485637
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2025-11-02 00:55:12 -0500

    Add User entity with fields and annotations

---
+++ backend/src/main/java/com/sote/FarmRa/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private Instant createdAt = Instant.now();
+}
