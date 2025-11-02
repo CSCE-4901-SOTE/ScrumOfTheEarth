d4d2748e6ce7ea66e18ee445ebd97019d2a08a4b
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2025-11-02 00:56:53 -0500

    Add RoleRepository for role entity
    Created RoleRepository for role entity management.

---
+++ backend/src/main/java/com/sote/FarmRa/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.example.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.backend.entity.Role;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+// Repository for working with the "role" table through the Role entity
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    // Automatically generates the query: SELECT * FROM role WHERE name = ?
+    Role findByName(String name);
+    @Query("SELECT r FROM Role r WHERE LOWER(r.name) = LOWER(:name)")
+    Role findByNameIgnoreCase(@Param("name") String name);
+}
