873c2c76146c4d3a71010dd500ec7a9b2b191989
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2025-11-11 23:16:03 -0600

    Enhance DashboardComponent with menu toggle
    Added CommonModule import and menu toggle functionality.

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink, RouterOutlet],
+  standalone: true,
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  // 🔹 Dropdown menu toggle
+  menuOpen = false;
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
