9ccb85242093ed71e03abe1f26bbde26f51dd684
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2026-02-05 01:18:21 -0600

    Refactor LoginResponse to include userId field

--- backend/src/main/java/com/sote/FarmRa/controller/LoginResponse.java
+++ backend/src/main/java/com/sote/FarmRa/controller/LoginResponse.java
@@ -1,14 +1,16 @@
-package com.sote.FarmRa.controller;
+package com.example.backend.controller;
+
+import java.util.UUID;
 
 public class LoginResponse {
-
+    private UUID userId;
     private String role;
 
-    public LoginResponse(String role) {
+    public LoginResponse(UUID userId, String role) {
+        this.userId = userId;
         this.role = role;
     }
 
-    public String getRole() {
-        return role;
-    }
+    public UUID getUserId() { return userId; }
+    public String getRole() { return role; }
 }
