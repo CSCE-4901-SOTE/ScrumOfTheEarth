ae33df3932b18540f0fc061257a9eefbd73df4d5
Zadeh, Blake <blakezadeh@my.unt.edu>
2026-03-05 01:22:07 -0600

    Create edit profile page ui only

--- frontend/src/app/edit-profile/edit-profile.component.ts
+++ frontend/src/app/edit-profile/edit-profile.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-profile.component.html',
-  styleUrl: './edit-profile.component.css',
+  styleUrls: ['./edit-profile.component.css'],
 })
 export class EditProfileComponent {
+  fullName = '';
+  dob = '';
+  farm = '';
+  phone = '';
+  userId = '';
 
+  //profile image
+  profileImageUrl: string | null = null;
+
+  onPickImage(fileInput: HTMLInputElement) {
+    const file = fileInput.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.profileImageUrl = String(reader.result);
+    };
+    reader.readAsDataURL(file);
+
+    fileInput.value = '';
+  }
+  //will add a save function later to save the profile changes to the backend
 }
