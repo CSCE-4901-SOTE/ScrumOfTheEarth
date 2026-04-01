fbe68176ea76a827c4fc512001051dc1d9b52647
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2026-04-01 13:21:43 -0500

    Replace HistoricalTrendsComponent with ReportPageComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HistoricalTrendsComponent } from './historical-trends/historical-trends.component';
+import { ReportPageComponent } from './report-page/report-page.component';
 import { AlertsComponent } from './alerts/alerts.component';
 import { MapSensorComponent } from './map-sensor/map-sensor.component';
 import { ContactsComponent } from './contacts/contacts.component';
@@ -13,7 +13,7 @@ export const routes: Routes = [
   { path: 'contacts', component: ContactsComponent },
   { path: 'signup-page', component: SignupPageComponent },
   { path: 'login-page', component: LoginPageComponent },
-  { path: 'historical-trends', component: HistoricalTrendsComponent },
+  { path: 'report-page', component: ReportPageComponent },
   { path: 'alert', component: AlertsComponent },
   { path: 'map-sensor', component: MapSensorComponent },
   { path: '', redirectTo: 'signup-page', pathMatch: 'full' },
