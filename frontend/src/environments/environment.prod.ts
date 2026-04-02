127b64eccccd274122eca127f5b408ee5cc2d896
cmccoy0603 <cvmccoy123@gmail.com>
2026-04-02 04:47:57 -0500

    Fix prod environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendUrl: "https://fmwcgyzmt8.execute-api.us-east-2.amazonaws.com",
+    backendUrl: "https://fmwcgyzmt8.execute-api.us-east-2.amazonaws.com/api",
 }
